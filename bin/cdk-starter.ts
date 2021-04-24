00a4734a3d07b3d92f58e7aea9dab3e20178238d
Borislav Hadzhiev <opmftw@gmail.com>
2021-04-24 10:40:35 +0300

    minor changes

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -3,8 +3,8 @@ import * as cdk from '@aws-cdk/core';
 import {MyCdkStack} from '../lib/cdk-starter-stack';
 
 const app = new cdk.App();
-new MyCdkStack(app, 'cdk-stack', {
-  stackName: 'cdk-stack',
+new MyCdkStack(app, 'my-cdk-stack', {
+  stackName: 'my-cdk-stack',
   env: {
     region: process.env.CDK_DEFAULT_REGION,
     account: process.env.CDK_DEFAULT_ACCOUNT,
